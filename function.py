96f3f3aff085ebc2f8801c4b8be2940895166fad
qlyu001 <qlyu001@ucr.edu>
2017-09-07 17:56:25 +0800

    finished the two sleeping function

---
+++ function.py
@@ -0,0 +1,39 @@
+import json
+from pprint import pprint
+epoch_length = 0.5
+
+def minutes_in_stage(epochstage):
+	#read in the epochstage and check the size
+	zero = epochstage.count(0) * epoch_length 
+	first = epochstage.count(1) * epoch_length 
+	second = epochstage.count(2) * epoch_length
+	third =  epochstage.count(3) * epoch_length
+	minuteinstage = {'0': zero, '1': first,'2': second, '3':third}
+	
+	return minuteinstage
+
+#this function could calculate the proportion of the data
+def proportion_in_stage(epochstage):
+
+	zero = epochstage.count(0) * epoch_length 
+	first = epochstage.count(1) * epoch_length 
+	second = epochstage.count(2) * epoch_length
+	third =  epochstage.count(3) * epoch_length
+	total = zero + first + second + third 
+	proportioninstage  = {'0': zero/(100*total), '1': first/(100*total),'2': second/(100*total), '3':third/(100*total)}
+	
+	print (proportioninstage )
+	return proportioninstage
+
+
+#json_file='a.json' 
+json_file='CAPStudy_subjectid216_visit1.json'
+
+json_data=open(json_file)
+data = json.load(json_data)
+#pprint(data)
+json_data.close()
+
+print ( "epochstage: ", data['epochstage'][1])
+minutes_in_stage(data['epochstage'])
+proportion_in_stage(data['epochstage'])
