1089ce49cc94666f47ba7dbcaf8ccbc21baa52b8
qlyu001 <qlyu001@ucr.edu>
2017-09-08 13:21:58 +0800

    add new function and change the coding styles

--- function.py
+++ function.py
@@ -6,28 +6,26 @@ epoch_length = 0.5
 
 def minutes_in_stage(epochstage):
 	#read in the epochstage and check the size
-	# This should be done in a loop. Whenever you are iterating something in words, you might as well use a loop
-	zero = epochstage.count(0) * epoch_length 
-	first = epochstage.count(1) * epoch_length 
-	second = epochstage.count(2) * epoch_length
-	third =  epochstage.count(3) * epoch_length
-	minuteinstage = {'0': zero, '1': first,'2': second, '3':third} #PEP8 standard variables should_be_like_this and notlikethis
-	return minuteinstage #You could just return the dictionary dirrectly here and save on a variable def
+	
+	total = 0
+	a = []
+	for x in range(0, 4):
+		temp = epochstage.count(x)*epoch_length
+		a.append(temp)
+		total += temp
+	
+	return  {'0': a[0], '1': a[1],'2': a[2], '3':a[3], 'total_sleep_time':total} 
 
 #this function could calculate the proportion of the data
 def proportion_in_stage(epochstage):
-#These function repeat each other, remeber the golden rule of programing, "dont repeat yourself". 
-# so, proportion_in_stage could just call minutes_in_stage. Feel free to add total_sleep_time as one of the dictionary keys to make this easier
 
-	zero = epochstage.count(0)
-	first = epochstage.count(1) 
-	second = epochstage.count(2)
-	third =  epochstage.count(3)
-	total = zero + first + second + third 
-	proportioninstage  = {'0': zero/(100*total), '1': first/(100*total),'2': second/(100*total), '3':third/(100*total)} #Why are you dividing by 100. Its already a proportion (range = [0,1])
-	return proportioninstage
+	temp = minutes_in_stage(epochstage)
+	total = temp['total_sleep_time']
+	return {'0': temp['0']/total, '1': temp['1']/total,'2': temp['2']/total, '3':temp['3']/total} 
 
-
+def sleep_efficiency(epochstage):
+	temp = minutes_in_stage(epochstage)
+	return (temp['1']+temp['2']+temp['3'])/temp['total_sleep_time']
 #json_file='a.json' 
 json_file='CAPStudy_subjectid216_visit1.json'
 
