94454dc00e5751cfbf98bf2a1a26ccdaf257b367
Benjamin Yetton <bdyetton@gmail.com>
2018-08-12 20:49:41 +0200

    added req

---
+++ mednickdb_pysleep/signal_features.py
@@ -0,0 +1,26 @@
+import numpy as np
+import scipy as sp
+
+
+def signal_envelope(sig):
+    return sp.signal.hilbert(sig)
+
+
+def mean_amplitude(sig):
+    return np.nanmean(sig)
+
+
+def max_amplitude(sig):
+    return np.nanmax(sig)
+
+
+def peak_freq(sig_spectrogram, freq_bins):
+    return freq_bins[np.argmax(np.nanmean(sig_spectrogram, axis=0))]
+
+
+
+
+#Rise time, etc
+
+
+
---
+++ mednickdb_pysleep/test_signal_features.py
@@ -0,0 +1,5 @@
+from mednickdb_pysleep.signal_features import *
+
+example_spindle =
+example_spindle_spectro =
+example_spindle_spectro_freqs =
