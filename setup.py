0bc005b67b49284f1c04ea1bb3fff4e3a0aa6144
bdyetton@gmail.com <bdyetton@gmail.com>
2018-10-28 22:59:36 -0700

    update to v0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='mednickdb_pysleep',
-    version='0.1dev',
+    version='0.2dev',
     packages=['mednickdb_pysleep'],
     license='MIT',
     long_description=open('README.md').read(),
